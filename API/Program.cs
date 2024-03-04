00d2cafc3bf9e070ae02acd8e6639a4d935b6e1a
Vitaliy Derda <derdavitaliy@gmail.com>
2024-03-04 18:31:13 +0200

    feat: added identity and app user

--- API/Program.cs
+++ API/Program.cs
@@ -1,6 +1,8 @@
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
 using API.Extensions;
 using API.Middleware;
+using Domain;
 using Persistence;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -8,6 +10,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllers();
 builder.Services.AddApplicationServices(builder.Configuration);
+builder.Services.AddIdentityServices(builder.Configuration);
 
 var app = builder.Build();
 
@@ -32,9 +35,11 @@ var services = scope.ServiceProvider;
 try
 {
     var context = services.GetRequiredService<DataContext>();
+    var userManager = services.GetRequiredService<UserManager<AppUser>>();
+
     await context.Database.MigrateAsync();
 
-    await Seed.SeedData(context);
+    await Seed.SeedData(context, userManager);
 }
 catch (Exception e)
 {
--- Persistence/DataContext.cs
+++ Persistence/DataContext.cs
@@ -1,9 +1,10 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Domain;
 
 namespace Persistence;
 
-public class DataContext : DbContext
+public class DataContext : IdentityDbContext<AppUser>
 {
     public DataContext(DbContextOptions options) : base(options)
     {
