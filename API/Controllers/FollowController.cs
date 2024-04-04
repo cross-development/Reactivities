e5428e11e3fbf96755b394d52d51a20fac1766e1
Vitaliy Derda <derdavitaliy@gmail.com>
2024-04-04 20:51:20 +0300

    feat: added follow controller

---
+++ API/Controllers/FollowController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Application.Followers;
+
+namespace API.Controllers;
+
+public class FollowController : BaseApiController
+{
+    [HttpGet("{username}")]
+    public async Task<IActionResult> Follow(string username)
+    {
+        return HandleResult(await Mediator.Send(new FollowToggle.Command { TargetUsername = username }));
+    }
+}
